3c2832c96fde8662ab855b04db5bd4b3cd6414ca
Edward Mixon <edwardchase@gatech.edu>
2018-06-09 16:20:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 def fizzbuzz(n)
-  if n % 3 == 0 
+  if n % 3 == 0 && n % 5 == 0 
+    "Fizzbuzz"
+  elsif n % 3 == 0 
     "Fizz"
   elsif
     n % 5 == 0 
     "Buzz"
-  elsif n % 3 == 0 && n % 5 == 0 
-    "Fizzbuzz"
+
   else 
     "nil"
   end 
